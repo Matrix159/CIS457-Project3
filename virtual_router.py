8becf392fb770038d0da4f322eb38bdc5c32a4ae
Joshua <joseldridge15@gmail.com>
2017-11-05 13:25:42 -0500

    Working on part 2

--- virtual_router.py
+++ virtual_router.py
@@ -15,7 +15,111 @@ def macToBinary(mac):
     """Convert MAC address to binary."""
     return binascii.unhexlify(mac.replace(':', ''))
 
+"""
+Finds MAC address of requested IP
+"""
+def findMac(IP):
+	#obtain list of addresses on the network
+	networkList = netifaces.interfaces()
+	print networkList
+	for iface in networkList:
+		addr = netifaces.ifaddresses(iface)[2][0]['addr']
+		mac = netifaces.ifaddresses(iface)[17][0]['addr']
+		print addr
+		print mac
+		#print socket.inet_ntoa(targetIP)
+		if addr == socket.inet_ntoa(IP):
+			return binascii.unhexlify(mac.replace(':', ''))
+	print  "MAC_NOT_FOUND"
+	return "MAC_NOT_FOUND"
+	
+	
+"""
+find next hop
+"""
+def findNextHop(iplist, destIp):
+	for entry in iplist:
+		splitEntry = entry.split(" ")
+		ipNumToMatch = splitEntry[0].split('/')
+		destIpSplit = destIP.split('.')
 
+		#checking 16 and 24 bit patterns
+		if ipNumToMatch[1] == 16:
+			ipSplit = ipNumToMatch[0].split('.')
+			if ipSplit[0:2] == destIpSplit[0:2]:
+				# Return (IP to send on, interface to send on)
+				return (splitEntry[1], splitEntry[2])
+		elif ipNumToMatch[1] == 24:
+			ipSplit = ipNumToMatch[0].split('.')
+			if ipSplit[0-2] == destIpSplit[0-2]:
+				# Return (IP to send on, interface to send on)
+				return (splitEntry[1], splitEntry[2])
+	return None
+
+"""
+gets routing tables and puts them into lists
+"""
+def getRoutingList():
+	table1 = open("r1-table.txt", "r")
+	table2 = open("r2-table.txt", "r")
+	listIP1 = table1.read().split("\n")
+	listIP2 = table2.read().split("\n")
+	print listIP1
+	print listIP2
+	
+def makeARPRequest(ethSourceMAC, arpHardwareType, arpProtocolType, arpHardwareSize, arpProtocolSize, arpSourceMAC, arpSourceIP, arpDestIP):
+	'''
+    "****************_ARP_REQUEST_*******************"
+    "************************************************"    
+    "****************_ETHERNET_FRAME_****************"
+    "Dest MAC:        ", binascii.hexlify(eth_detailed[0])
+    "Source MAC:      ", binascii.hexlify(eth_detailed[1])
+    "Type:            ", binascii.hexlify(eth_detailed[2])
+    "************************************************"
+    "******************_ARP_HEADER_******************"
+    "Hardware type:   ", binascii.hexlify(arp_detailed[0])
+    "Protocol type:   ", binascii.hexlify(arp_detailed[1])
+    "Hardware size:   ", binascii.hexlify(arp_detailed[2])
+    "Protocol size:   ", binascii.hexlify(arp_detailed[3])
+    "Opcode:          ", binascii.hexlify(arp_detailed[4])
+    "Source MAC:      ", binascii.hexlify(arp_detailed[5])
+    "Source IP:       ", socket.inet_ntoa(arp_detailed[6])
+    "Dest MAC:        ", binascii.hexlify(arp_detailed[7])
+    "Dest IP:         ", socket.inet_ntoa(arp_detailed[8])
+    "************************************************\n"    
+	'''
+	destMac = "\xFF\xFF\xFF\xFF\xFF\xFF"
+	ethType = "\x08\x06"
+	arpOpCode = "\x00\x01"
+	
+	# pack back to binary
+    new_eth_header = struct.pack("6s6s2s", destMac, ethSourceMAC, ethType)
+    new_arp_header = struct.pack("2s2s1s1s2s6s4s6s4s", arpHardwareType, arpProtocolType, arpHardwareSize, arpProtocolSize, arpOpCode, arpSourceMAC, arpSourceIP, arpDestIP)
+	
+	return new_eth_header + new_arp_header
+	
+"""
+Creates ARP header
+"""
+def makeArpHeader(reply, hwareType, pcType, hwareSize, pcSize, srcMac, srcIp, destMac, destIp):
+
+	if reply is True:
+		opCode = '\x00\x02'
+
+	#this is an ARP request
+	else:
+		opCode = '\x00\x01'
+		nextHop = findNextHop(listIP1, destIp)
+		if nextHop is None:
+			nextHop = findNextHop(listIP2, destIp)
+			#if nextHop is False: send error message.  Part three stuff
+			#TODO: Since this means we need to jump to r2's network, we need to do an ARP request
+
+	arpHeader = struct.pack("2s2s1s1s2s6s4s6s4s", hwareType, pcType, hwareSize, pcSize,
+		opCode , srcMac, srcIp, destMac, destIp)
+
+	return arpHeader
+	
 def main(argv):
     try: 
         s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x003))
@@ -66,17 +170,26 @@ def main(argv):
             source_IP = socket.inet_ntoa(arp_detailed[6])
             dest_IP = socket.inet_ntoa(arp_detailed[8])
 
-            # get list of network interfaces 
-            net_list = netifaces.interfaces()
+            ## get list of network interfaces 
+            #net_list = netifaces.interfaces()
             
-            # loop over interfaces until find one that matches dest
+			# Look up MAC address in interfaces
+			dest_MAC = findMac(dest_IP)
+			
+            '''# loop over interfaces until find one that matches dest
             for net in net_list:
                 net_IP = netifaces.ifaddresses(net)[2][0]['addr']
                 net_MAC = netifaces.ifaddresses(net)[17][0]['addr']
 
                 if dest_IP == net_IP:
                     dest_MAC = net_MAC
-            
+            '''
+			
+			# CALL MAKE ARP HEADER HERE? 
+			#new_arp_header = makeArpHeader(True, arp_detailed[0], arp_detailed[1], arp_detailed[2], arp_detailed[3]
+			#	, eth_detailed[0], arp_detailed[8], macToBinary(dest_MAC), arp_detailed[6])
+			
+			
             # tuples are immutable in python, copy to list
             new_eth_detailed_list = list(eth_detailed)
             new_arp_detailed_list = list(arp_detailed)
@@ -107,7 +220,7 @@ def main(argv):
             new_arp_header = struct.pack("2s2s1s1s2s6s4s6s4s", *new_arp_detailed)
 
             # combine ethernet and arp headers
-            new_packet = new_eth_header + new_arp_header      
+            new_packet = new_eth_header + new_arp_header     
 
             ethernet_header = new_packet[0:14]
             ethernet_detailed = struct.unpack("!6s6s2s", ethernet_header)
